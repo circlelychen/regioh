eb8fdba5d1f6d0aa258732816904e217e878eeae
circlelychen <truecirclely@gmail.com>
2013-05-16 14:38:38 +0800

    return linkiedin for v2_register

--- regioh/api_view.py
+++ regioh/api_view.py
@@ -90,7 +90,6 @@ def v2_register():
         register_email(linkedin_id, user_email, pubkey, token)
 
         item['pubkey'] = pubkey
-        del item['linkedin_id']
         del item['oauth_token']
         del item['oauth_token_secret']
         del item['token']

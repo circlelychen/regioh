acfb8ba192df84b44be2b2d4b1d9b2dfc43313ee
circlelychen <truecirclely@gmail.com>
2013-05-21 20:15:07 +0800

    add acccount_init and benchmar

---
+++ account_init.py
@@ -0,0 +1,95 @@
+import logging
+import os
+import sys
+import requests
+import json
+import urlparse
+from regioh.default_config import GD_CLIENT_ID
+from regioh.default_config import GD_CLIENT_SECRET
+from regioh.default_config import GD_REDIRECT_URL
+from regioh.default_config import GD_LOGIN_HINT
+
+
+def _access_v2_token(client_id, client_secret, authorization_code, redirect_uri):
+    access_token_url = 'https://accounts.google.com/o/oauth2/token'
+    params = {"client_id": client_id,
+              "client_secret": client_secret,
+              "code": authorization_code,
+              "grant_type": u"authorization_code",
+              "redirect_uri":redirect_uri}
+
+    resp = requests.request('POST', access_token_url, data=params)
+
+    if resp.status_code == 200:
+        return resp.json()
+    else:
+        print resp.status_code
+        print resp.content
+        return None
+
+def get_v2_access_token(paras):
+    if len(paras) <= 0:
+        sys.exit("Usage: get_v2_access_token <authorization_code>")
+
+    client_id = GD_CLIENT_ID
+    client_secret = GD_CLIENT_SECRET
+    redirect_uri = GD_REDIRECT_URL
+    authorization_code = paras[0]
+    content_to_file = _access_v2_token(client_id, client_secret,
+                                       authorization_code, redirect_uri)
+    return content_to_file
+
+def get_v2_request_url(paras):
+    client_id = GD_CLIENT_ID
+    client_secret = GD_CLIENT_SECRET
+    redirect_url = GD_REDIRECT_URL
+    print "Client ID : {0}".format(client_id)
+    print "Client Secret : {0}".format(client_id)
+
+    authorize_url = 'https://accounts.google.com/o/oauth2/auth'
+    scope = "https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.appdata+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive"
+    state = "DCEEFWF45453sdffef424"
+    result = "{0}?response_type={5}&client_id={1}&scope={2}&state={3}&redirect_uri={4}".format(authorize_url, client_id, scope, state, redirect_url, "code")
+    return result
+
+def create_v2_token(params):
+    fout = params[0]
+    ticket_url = get_v2_request_url([])
+    print "Go to the following link in your browser:\n"
+    print "{0} \n".format(ticket_url)
+    accepted = 'n'
+    while accepted.lower() == 'n':
+        accepted = raw_input('Have you authorized me? (y/n) ')
+        oauth_verifier = raw_input('What is the authorization code? ')
+
+    content = get_v2_access_token([oauth_verifier])
+    with open(fout, 'wb') as fout:
+        content['client_id'] = GD_CLIENT_ID
+        content['client_secret'] = GD_CLIENT_SECRET
+        json.dump(content, fout, indent=2)
+
+def doCommand(cmd, *args):
+    if cmd in globals():
+        return globals()[cmd](list(args))
+    else:
+        raise LookupError("Command not recognised")
+
+def main(argv):
+    if len(argv) <= 2:
+        out_sample = {"access_token": "aa", "id_token": "aa",
+                      "expires_in": "aa", "token_type": "aa",
+                      "client_id": "aa", "client_secret": "aa",
+                      "refresh_token": "aa" }
+        sys.exit("Usage: {0} <{1}> <output> \n## output sample ##\n{2}".format(
+            argv[0],
+            '|'.join([
+                'create_v2_token',
+            ]),
+            json.dumps(out_sample, indent=2)))
+    try:
+        doCommand(argv[1], *argv[2:])
+    except Exception as e:
+        logging.basicConfig()
+        logging.getLogger().exception(e)
+if __name__ =='__main__':
+    sys.exit(main(sys.argv))
---
+++ benchmark.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+import os
+import sys
+import logging
+import tempfile
+import string
+import random
+import threading
+import json
+from Queue import Queue
+
+from regioh.default_config import PROJECT_ROOT
+from regioh import api_helper
+
+path = os.getcwd()
+if path not in sys.path:
+    sys.path.append(path)
+
+from gdapi.gdapi import GDAPI
+q = Queue()
+
+accounts = [#'cipherbox@cloudioh.com.cred.json'
+            #'developer@cloudioh.com.cred.json',
+            #'apple110531@gmail.com.cred.json',
+            'banana110531@gmail.com.cred.json',
+            #'cherry110531@gmail.com.cred.json',
+            #'justin_jan@cloudioh.com.cred.json',
+            #'jjcipher@gmail.com.cred.json',
+            #'howard_chen@cloudioh.com.cred.json'
+           ]
+#the following content depicts 100 connections for a person
+contact_file = 'connection_contacts'
+CONNECT_NUM = 0
+
+randstr = lambda x: u''.join(
+    random.choice(string.ascii_lowercase + string.digits) for x in xrange(x))
+
+local_data = threading.local()
+
+def worker(folder_id, queue, ga, contacts):
+    while True:
+        signal = queue.get()
+
+        # 1. insert "contacts file" into GD
+        #folder_id = api_helper.create_folder(folder_id,
+        #                          ''.join([threading.currentThread().getName(),'-',
+        #                                   str(signal)
+        #                                  ])
+        #                         )
+
+        # 2. insert "contacts file" into GD
+        _, temp_path = tempfile.mkstemp()
+        with open(temp_path, "wb") as fout:
+            json.dump(contacts, fout, indent=2)
+        file_id = ga.create_or_update_file(folder_id,
+                                           os.path.join(os.path.dirname(PROJECT_ROOT),
+                                                        'accounts',
+                                                        contact_file),
+                                           ''.join([threading.currentThread().getName(),
+                                                    '-', str(signal)
+                                                   ])
+                                          )
+        os.unlink(temp_path)
+
+        #for i in xrange(CONNECT_NUM):
+        #    # 3. download file
+        #    _, temp_path = tempfile.mkstemp()
+        #    if api_helper.download_file(file_id, temp_path):
+        #        with open(temp_path, "wb") as fout:
+        #            json.dump(contacts, fout, indent=2)
+        #        # 4. update file
+        #        api_helper.update_file(file_id, temp_path)
+
+
+        #ga.create_file(folder_id, temp_path,
+        #            ''.join([threading.currentThread().getName(),
+        #                    '-', str(signal)]))
+        queue.task_done()
+
+def main(argv):
+    logger = logging.getLogger('gdapi')
+    logger.addHandler(logging.StreamHandler())
+    logger.setLevel(logging.DEBUG)
+
+    # master accoutn create 'testreg' and share with other 'slaves'
+    #ga = GDAPI(os.path.join(os.path.dirname(PROJECT_ROOT), 'accounts',
+    #                        accounts[0]))
+    #folder_id = ga.create_folder(u'root', 'testreq')
+    with open(os.path.join(os.path.dirname(PROJECT_ROOT),
+                           'accounts',
+                           contact_file),
+              'rb') as f:
+        contacts = json.load(f,)
+        print contacts
+
+
+    for account in accounts:
+        ga = GDAPI(os.path.join(os.path.dirname(PROJECT_ROOT), 'accounts', account))
+        folder_id = ga.create_folder(u'root', 'testreq')
+        t = threading.Thread(target=worker,
+                             args=(folder_id, q, ga, contacts,))
+        t.setDaemon(True)
+        t.start()
+
+    from timeit import default_timer as timer
+    start = timer()
+    for x in xrange(1):
+        q.put(x)
+    q.join()
+    logger.info('Total: %r sec', timer() - start)
+
+if __name__ == '__main__':
+    main(sys.argv)

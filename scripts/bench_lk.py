39f641c67dae486b9d05814fca5c956687094a7b
circlelychen <truecirclely@gmail.com>
2013-05-22 18:02:32 +0800

    add bench_lk.py

--- scripts/bench_lk.py
+++ scripts/bench_lk.py
@@ -9,72 +9,45 @@ import threading
 import json
 from Queue import Queue
 
-from regioh.default_config import PROJECT_ROOT
-from regioh import api_helper
-
 path = os.getcwd()
 if path not in sys.path:
     sys.path.append(path)
 
-from gdapi.gdapi import GDAPI
+from regioh.default_config import PROJECT_ROOT
+from regioh.api_helper import get_linkedin_connection
 q = Queue()
 
-accounts = ['cipherbox@cloudioh.com.cred.json',
-            'developer@cloudioh.com.cred.json',
-            'apple110531@gmail.com.cred.json',
-            'banana110531@gmail.com.cred.json',
-            'cherry110531@gmail.com.cred.json',
-            'justin_jan@cloudioh.com.cred.json',
-            'jjcipher@gmail.com.cred.json',
-            'howard_chen@cloudioh.com.cred.json'
-           ]
-#the following content depicts 100 connections for a person
-contact_file = 'connection_contacts'
-CONNECT_NUM = 0
+accounts = {
+    'truecirclely@gmail.com': {
+        'id': 'rdlsVH788A',
+        'oauth_token': 'e64bac71-cb18-4865-bfdd-554f7426eb46',
+        'oauth_token_secret': '3dc5ffab-9729-4478-983a-f2b95f1a8ce8'
+    },
+    'apple110531@gmail.com': {
+        'id': 'AryLrgEqrF',
+        'oauth_token': 'dedfb682-f57a-4d66-9e10-6ef6c7d72bb5',
+        'oauth_token_secret': '8644996e-e534-412b-bcd2-463a1ad5d2d2'
+    },
+    'banana110531@gmail.com': {
+        'id': 'tmoijVoPVd',
+        'oauth_token': '6ba8a7ac-bf3d-495d-8f1f-6a0d7994074d',
+        'oauth_token_secret': '1d1e2754-0d24-4cf4-bd70-f364f55c4ac4'
+    }
+}
 
-randstr = lambda x: u''.join(
-    random.choice(string.ascii_lowercase + string.digits) for x in xrange(x))
 
-local_data = threading.local()
 
-def worker(folder_id, queue, ga, contacts):
+def worker(queue, account):
     while True:
         signal = queue.get()
 
-        # 1. insert "contacts file" into GD
-        #folder_id = api_helper.create_folder(folder_id,
-        #                          ''.join([threading.currentThread().getName(),'-',
-        #                                   str(signal)
-        #                                  ])
-        #                         )
-
-        # 2. insert "contacts file" into GD
-        _, temp_path = tempfile.mkstemp()
-        with open(temp_path, "wb") as fout:
-            json.dump(contacts, fout, indent=2)
-        file_id = ga.create_or_update_file(folder_id,
-                                           os.path.join(os.path.dirname(PROJECT_ROOT),
-                                                        'accounts',
-                                                        contact_file),
-                                           ''.join([threading.currentThread().getName(),
-                                                    '-', str(signal)
-                                                   ])
-                                          )
-        os.unlink(temp_path)
-
-        #for i in xrange(CONNECT_NUM):
-        #    # 3. download file
-        #    _, temp_path = tempfile.mkstemp()
-        #    if api_helper.download_file(file_id, temp_path):
-        #        with open(temp_path, "wb") as fout:
-        #            json.dump(contacts, fout, indent=2)
-        #        # 4. update file
-        #        api_helper.update_file(file_id, temp_path)
-
-
-        #ga.create_file(folder_id, temp_path,
-        #            ''.join([threading.currentThread().getName(),
-        #                    '-', str(signal)]))
+        status, jobj = get_linkedin_connection(
+            account['oauth_token'],
+            account['oauth_token_secret'])
+        if status != 200:
+            logger = logging.getLogger('gdapi')
+            logger.debug(status)
+            logger.debug(jobj)
         queue.task_done()
 
 def main(argv):
@@ -85,23 +58,9 @@ def main(argv):
     logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.DEBUG)
 
-    # master accoutn create 'testreg' and share with other 'slaves'
-    #ga = GDAPI(os.path.join(os.path.dirname(PROJECT_ROOT), 'accounts',
-    #                        accounts[0]))
-    #folder_id = ga.create_folder(u'root', 'testreq')
-    with open(os.path.join(os.path.dirname(PROJECT_ROOT),
-                           'accounts',
-                           contact_file),
-              'rb') as f:
-        contacts = json.load(f,)
-        print contacts
-
-
-    for account in accounts:
-        ga = GDAPI(os.path.join(os.path.dirname(PROJECT_ROOT), 'accounts', account))
-        folder_id = ga.create_folder(u'root', 'testreq')
+    for key in accounts:
         t = threading.Thread(target=worker,
-                             args=(folder_id, q, ga, contacts,))
+                             args=(q,accounts[key]))
         t.setDaemon(True)
         t.start()
 

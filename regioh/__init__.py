7528a9f2c00f3ded50e650c3b976c187c8cce4ac
circlelychen <truecirclely@gmail.com>
2013-05-24 01:35:38 +0800

    add setup_threads procedure

--- regioh/__init__.py
+++ regioh/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import threading
 import os
 
 app = Flask(__name__)
 app.config.from_object('regioh.default_config')
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 app.secret_key = '\xe1\r#\x99\xfb\x10T\x11\x07\x8a+\x00\xbe\xe7$\xa1\x86\x05\x97\xf0\xe0\xa7\xd9\xed'
+
+
+import regioh.api_view
+import regioh.views
 
 #@app.before_first_request
 def setup_logging():
@@ -51,8 +56,35 @@ def setup_logging():
     else:
         app.logger.debug("Debug mode")
 
-setup_logging()
+def setup_threads():
+    if not app.debug and not app.config['TESTING']:
+        from gdapi.gdapi import GDAPI
+        from default_config import MASTER
+        from default_config import ACCOUNTS
+        from default_config import PROJECT_ROOT
+        from tasks import worker
+        from tasks import queue
+        # master create shared folder
+        master_ga = GDAPI(os.path.join(os.path.dirname(PROJECT_ROOT),
+                                        'accounts',
+                                        MASTER))
+        root_id = master_ga.create_folder(u'root', os.path.basename(__name__))
+        # initial worker threads for each account
+        for account in ACCOUNTS:
+            tokens = account.split('.')
+            perm_id = master_ga.make_user_writer_for_file(root_id,
+                                                        ".".join([tokens[0], tokens[1]]))
+            ga = GDAPI(os.path.join(os.path.dirname(PROJECT_ROOT),
+                                    'accounts',
+                                    account))
+            contacts = {'aaa': 'aasa'}
+            t = threading.Thread(target=worker,
+                                 name = account,
+                                 args=(root_id, queue, ga, contacts,))
+            t.setDaemon(True)
+            t.start()
 
-import regioh.api_view
-import regioh.views
+setup_logging()
+setup_threads()
+
 
